46ea8eddf5d58329f154a8c106272643a9505e35
shilingling00 <2460047746@qq.com>
2019-04-19 17:14:55 +0800

    伸缩图片 flex vh classlist

--- day5伸缩图片/index.js
+++ day5伸缩图片/index.js
@@ -9,6 +9,8 @@ list.forEach(item=>{
 
 function open() {
     this.classList.toggle('open');
+    //classList 属性返回元素的类名，作为 DOMTokenList 对象。
+    // 该属性用于在元素中添加，移除及切换 CSS 类。
 }
 function openactive(e) {
     console.log(e.propertyName);
