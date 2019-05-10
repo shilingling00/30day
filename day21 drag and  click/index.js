cc6d197ee242210330b15ddc834c8630bb98d945
shilingling00 <2460047746@qq.com>
2019-05-10 17:24:47 +0800

    拖拽

---
+++ day21 drag and  click/index.js
@@ -0,0 +1,24 @@
+let item=document.querySelector('.items');
+let isdown=false;
+let startX;
+let scrollLeft;
+item.addEventListener('mousedown',function f(e) {
+    isdown=true;
+    item.classList.add('active');
+    startX=e.pageX-item.offsetLeft;
+    scrollLeft=item.scrollLeft;
+});
+item.addEventListener('mouseleave',function () {
+    isdown=false;
+    this.classList.remove('active');
+});
+item.addEventListener('mouseup',function () {
+    isdown=false;
+    this.classList.remove('active')
+});
+item.addEventListener('mousemove',function (e) {
+   if(!isdown) return;
+   let x=e.pageX-item.offsetLeft;
+    let walk=(x-startX)*3;
+    item.scrollLeft=scrollLeft-walk;
+});
